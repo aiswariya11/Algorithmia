536b560b7b513bf75d196aebe60bba52d8fa78b7
fasalmbt <>
2020-10-09 20:48:02 +0530

    Added binary search

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,3 +1,13 @@
+'''
+In binary search,
+
+ - Compare x with the middle element.
+ - If x matches with the middle element, we return the mid index.
+ - Else if x is greater than the mid element, then x can only lie in the right (greater) half subarray after the mid element. 
+   Then we apply the algorithm again for the right half.
+ - Else if x is smaller, the target x must lie in the left (lower) half. So we apply the algorithm for the left half.
+'''
+
 def binary(a,f,l,x):
 	if l >= f:
 		m = (l+f) // 2
@@ -19,4 +29,4 @@ result = binary(a,0,len(a)-1,x)
 if result != -1:
 	print("Element is present at "+str(result)+"th index")
 else:
-	print("Sorry! element not present")
+	print("Sorry! element is not present")
